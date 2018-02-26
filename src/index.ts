f00e736757b74c79e70e0dc1f310cd27a2f06ab7
morninng <mr_morninng_star_dash@yahoo.co.jp>
2018-02-26 11:10:16 +0900

    put comment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
-console.log("Hello! Node.js × TypeScript");
 
 interface TestIf {
     aaa: string;
@@ -12,24 +11,26 @@ interface TestIf {
 
 const testSubject = new ReplaySubject<TestIf>(1);
 const test$ = testSubject.map((data)=>{
-    console.log("map");
+    console.log("root map");
     return {
         aaa: data.aaa + "1",
         bbb: data.bbb + "2"
     }
 }).do(()=>{
-    console.log("do");
+    console.log("root do");
 })
 
 
 function get_aaa(): Observable<string>{
     return test$.map((data: TestIf)=>{
+        console.log('get_aaa map')
         return data.aaa;
     })
 }
 
 function get_bbb(): Observable<string>{
     return test$.map((data: TestIf)=>{
+        console.log('get_bbb map')
         return data.bbb;
     })
 }
